fea52186048a1f3f7b1fd1c5b76ac9c1852c8542
Jennifer <jen.yiu.90@gmail.com>
2019-03-12 11:02:59 -0700

    cchange view in App.js

---
+++ client/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import Home from './js/screens/Home';
+// import RootStackNavigator from './navigation/RootStackNavigator';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Home />
+      </View>
+    );
+  }
+}

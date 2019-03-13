531da383423cd810c7ba669903938463b51bba37
alexfts <alxfetisova@gmail.com>
2019-03-13 15:09:24 -0700

    Add apollo dependencies

--- client/js/App.js
+++ client/js/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import { Text, View, StatusBar } from 'react-native';
 // import RootStackNavigator from './navigation/RootStackNavigator';
 import Login from './screens/Login';
+import client from './config/api';
+import { ApolloProvider } from 'react-apollo';
 
 export default class App extends Component {
   render() {
     StatusBar.setBarStyle('light-content', true);
 
     return (
-      <View>
-        <Text>Welcome to React Native!</Text>
-        <Login />
-        {/* <RootStackNavigator /> */}
-      </View>
+      <ApolloProvider client={client}>
+        <View>
+          <Text>Welcome to React Native!</Text>
+          <Login />
+          {/* <RootStackNavigator /> */}
+        </View>
+      </ApolloProvider>
     );
   }
 }

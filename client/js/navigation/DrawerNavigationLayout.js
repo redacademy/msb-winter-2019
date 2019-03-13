e1c7bbeb81dc4570d00723ee81674fa2b71887ec
Jennifer <jen.yiu.90@gmail.com>
2019-03-13 13:59:04 -0700

    create screens for Contact and Profile

--- client/js/navigation/DrawerNavigationLayout.js
+++ client/js/navigation/DrawerNavigationLayout.js
@@ -1,1 +1,108 @@
-// drawer navigation
+import React from 'react';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Image } from 'react-native';
+
+import ProfileScreen from '../screens/Profile';
+import FavBeersScreen from '../screens/FavBeers';
+import FavEventsScreen from '../screens/FavEvents';
+import ContactScreen from '../screens/Contact';
+import { colors } from '../config/styles';
+import { sharedNavigationOptions } from './config';
+
+const ProfileStack = createStackNavigator(
+  {
+    Profile: ProfileScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+const FavBeersStack = createStackNavigator(
+  {
+    FavBeers: FavBeersScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+const FavEventsStack = createStackNavigator(
+  {
+    FavEvents: FavEventsScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+const ContactStack = createStackNavigator(
+  {
+    Contact: ContactScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+
+export default createDrawerNavigator(
+  {
+    Profile: ProfileStack,
+    FavBeers: FavBeersStack,
+    Events: EventsStack,
+    Stores: StoresStack
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        console.log('>>>>>>>>>', navigation);
+
+        let icon;
+        if (routeName === 'Home') {
+          icon = focused
+            ? require('../assets/images/Navigation/home_icon_active.png')
+            : require('../assets/images/Navigation/home_icon_inactive.png');
+        } else if (routeName === 'Card') {
+          icon = focused
+            ? require('../assets/images/Navigation/card_icon_active.png')
+            : require('../assets/images/Navigation/card_icon_inactive.png');
+        }
+        if (routeName === 'Beers') {
+          icon = focused
+            ? require('../assets/images/Navigation/beers_icon_active.png')
+            : require('../assets/images/Navigation/beer_icon_inactive.png');
+        } else if (routeName === 'Events') {
+          icon = focused
+            ? require('../assets/images/Navigation/events_icon_active.png')
+            : require('../assets/images/Navigation/event_icon_inactive.png');
+        } else if (routeName === 'Stores') {
+          icon = focused
+            ? require('../assets/images/Navigation/stores_icon_active.png')
+            : require('../assets/images/Navigation/stores_icon_inactive.png');
+        }
+        return <Image source={icon} style={{ height: 30, width: 30 }} />;
+      }
+    }),
+
+    tabBarOptions: {
+      activeTintColor: colors.brand,
+      inactiveTintColor: colors.black,
+      labelStyle: {
+        fontSize: 12,
+        fontFamily: 'Verdana',
+        lineHeight: 0
+      },
+      style: {
+        backgroundColor: '#fff',
+        height: 55,
+        paddingTop: 8
+      }
+    }
+  }
+);
---
+++ client/js/screens/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const Contact = props => {
+  return <Text>This is Contact.</Text>;
+};
+
+Contact.propTypes = {};
+
+export default Contact;

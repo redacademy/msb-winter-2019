56917a4577ee2804716422dd62aefae94557cfa2
Kyle_Tecson <kyltecs@gmail.com>
2019-03-15 16:17:23 -0700

    fix merge conflict

--- client/js/screens/Card/CardContainer.js
+++ client/js/screens/Card/CardContainer.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-import { View } from "react-native";
+import PropTypes from "prop-types";
 
 import Card from "./Card";
-import styles from "./styles";
-=======
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
-import Card from './Card';
->>>>>>> develop
 
 class CardContainer extends Component {
   render() {

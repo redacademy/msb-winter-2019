64376e2b67aba248962927402ea51299e28085a3
Jennifer <jen.yiu.90@gmail.com>
2019-03-15 10:16:15 -0700

    edit static header to storescontainer

--- client/js/screens/Stores/StoresContainer.js
+++ client/js/screens/Stores/StoresContainer.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Image, Platform, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Stores from './Stores';
 import styles from './styles';
+import { center, row } from '../../config/styles';
 
 class StoresContainer extends Component {
   static navigationOptions = {
-    title: (
-      <Image source={require('../../assets/images/Logos/msb_logo_white.png')} />
+    // title: (
+    //   <Image source={require('../../assets/images/Logos/msb_logo_white.png')} />
+    // ),
+    headerLeft: (
+      <TouchableOpacity
+      // style={Styles.headerButton}
+      // onPress={() => navigation.toggleDrawer()}
+      >
+        <Icon
+          name={Platform.select({
+            ios: 'ios-arrow-forward',
+            android: 'md-arrow-forward'
+          })}
+          size={25}
+          color={'green'}
+        />
+      </TouchableOpacity>
     ),
     headerTintColor: '#fff',
-    headerTitleStyle: { ...Header }
+    headerTitleStyle: { ...row, ...center }
   };
   render() {
     return <Stores />;

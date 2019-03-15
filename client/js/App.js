00e28bb7a8f215310837729ce75f55593a431503
alexfts <alxfetisova@gmail.com>
2019-03-15 11:10:21 -0700

    Remove extra comment

--- client/js/App.js
+++ client/js/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Text, View, StatusBar } from 'react-native';
-// import RootStackNavigator from './navigation/RootStackNavigator';
 import Login from './screens/Login';
 import client from './config/api';
 import { ApolloProvider } from 'react-apollo';

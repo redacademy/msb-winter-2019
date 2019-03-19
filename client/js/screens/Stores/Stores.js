0e693bbd2cc36b74a6149cfc8149c1cc150c9b86
Kyle_Tecson <kyltecs@gmail.com>
2019-03-19 15:34:32 -0700

    testing maps

--- client/js/screens/Stores/Stores.js
+++ client/js/screens/Stores/Stores.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import PropTypes from "prop-types";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 import styles from "./styles";
 
@@ -9,9 +9,29 @@ const Stores = ({ stores }) => {
   console.log(stores);
 
   return (
-    <View>
-      <MapView provider={PROVIDER_GOOGLE} style={styles.mapContainer} />
-      <Text>This is Stores.</Text>
+    <View style={styles.container}>
+      <View style={styles.mapContainer}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            longitude: -123.099305,
+            latitude: 49.26473,
+            latitudeDelta: 0.004,
+            longitudeDelta: 0.004
+          }}
+          onRegionChange={}
+        >
+        {stores.map((store) =>(
+          <Marker 
+          coordinate={}
+          />
+        ))}
+        </MapView>
+      </View>
+      <View style={styles.listContainer}>
+        <Text>This is Stores.</Text>
+      </View>
     </View>
   );
 };
--- client/js/screens/Stores/styles.js
+++ client/js/screens/Stores/styles.js
@@ -1,8 +1,20 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%"
+  },
   mapContainer: {
     height: "50%",
+    width: "100%"
+  },
+  map: {
+    height: "100%",
+    width: "100%"
+  },
+  listContainer: {
+    height: "100%",
     width: "100%"
   }
 });

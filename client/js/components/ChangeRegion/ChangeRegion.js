a00c5a21e989e3a762a57a00fcdfba5e0743f8b3
Kyle_Tecson <kyltecs@gmail.com>
2019-03-20 14:18:26 -0700

    add subheader to stores page

--- client/js/components/ChangeRegion/ChangeRegion.js
+++ client/js/components/ChangeRegion/ChangeRegion.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import PropTypes from "prop-types";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import SubHeader from "../Subheader";
 
 import styles from "./styles";
 
@@ -35,6 +36,7 @@ class ChangeRegion extends Component {
     const { stores } = this.props;
     return (
       <View style={styles.container}>
+        <SubHeader>Stores</SubHeader>
         <View style={styles.mapContainer}>
           {this.state.region.longitude && (
             <MapView

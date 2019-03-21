a2d67a917ef85e14a072800790d78976bc0f8062
nejmal <lam.jennifer.ky@gmail.com>
2019-03-21 09:59:08 -0700

    Remove commented code.

--- client/js/screens/AllEvents/AllEvents.js
+++ client/js/screens/AllEvents/AllEvents.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import { View, Text } from 'react-native';
-// import PropTypes from 'prop-types';
-
-// import styles from './styles';
-
-// const AllEvents = () => {
-//   return <Text>This is AllEvents.</Text>;
-// };
-
-// AllEvents.propTypes = {};
-
-// export default AllEvents;
-
 import PropTypes from 'prop-types';
 
 const AllEvents = ({ navigation }) => {

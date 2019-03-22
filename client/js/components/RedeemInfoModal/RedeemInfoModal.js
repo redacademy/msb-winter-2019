8e449132408436086542db158cfc7cd4a479460d
Kyle_Tecson <kyltecs@gmail.com>
2019-03-22 14:15:25 -0700

    model styled but no image yet

--- client/js/components/RedeemInfoModal/RedeemInfoModal.js
+++ client/js/components/RedeemInfoModal/RedeemInfoModal.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import { withNavigation } from 'react-navigation';
-import styles from './styles';
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { withNavigation } from "react-navigation";
+import styles from "./styles";
 
 class RedeemInfoModal extends Component {
   render() {
     const { navigation } = this.props;
-    const user = navigation.getParam('user');
-    const reward = navigation.getParam('reward');
+    const user = navigation.getParam("user");
+    const reward = navigation.getParam("reward");
     return (
       <View style={styles.container}>
-        <Text>You are now redeeming</Text>
-        <Text>{`${reward.points} stamps`}</Text>
-        <Text>{reward.title}</Text>
-        <Button
+        <Text style={styles.heading}>You are now redeeming</Text>
+        <Text style={styles.redeem}>{`${reward.points} stamps`}</Text>
+        <Text style={styles.rewardTitle}>{reward.title}</Text>
+
+        <TouchableOpacity
+          style={styles.redeemButton}
           onPress={() => {
-            navigation.navigate('RedeemBarcode', { user, reward });
+            navigation.navigate("RedeemBarcode", { user, reward });
           }}
-          title="Got it! Redeem"
-        />
-        <Button
-          onPress={() => {
-            navigation.goBack();
-          }}
-          title="Not right now"
-        />
+        >
+          <Text style={styles.redeemText}>Got it! Redeem</Text>
+        </TouchableOpacity>
+        <View style={styles.cancelContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Text style={styles.cancel}>Not right now</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
--- client/js/components/RedeemInfoModal/styles.js
+++ client/js/components/RedeemInfoModal/styles.js
@@ -1,10 +1,47 @@
-import { StyleSheet } from 'react-native';
-import { h1, colors, container } from '../../config/styles';
+import { StyleSheet } from "react-native";
+import { h1, colors, container, h2, fonts } from "../../config/styles";
 
 const styles = StyleSheet.create({
   container: {
     ...container,
-    height: '100%'
+    height: "100%"
+  },
+  heading: {
+    ...h2,
+    color: colors.neutralLight
+  },
+  redeem: {
+    ...h1
+  },
+  rewardTitle: {
+    fontFamily: fonts.primaryBold,
+    fontSize: fonts.md,
+    marginBottom: 20
+  },
+
+  redeemButton: {
+    padding: 20,
+    backgroundColor: colors.brand,
+    borderRadius: 10,
+    marginTop: 20,
+    marginBottom: 20
+  },
+
+  redeemText: {
+    color: colors.white,
+    fontFamily: fonts.primaryBold,
+    fontSize: fonts.lg
+  },
+
+  cancel: {
+    fontFamily: fonts.primarySemi,
+    fontSize: fonts.md,
+    color: colors.neutralDark
+  },
+
+  cancelContainer: {
+    borderBottomWidth: 2,
+    borderBottomColor: colors.neutralLight
   }
 });
 

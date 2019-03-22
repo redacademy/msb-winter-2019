7f69cf43cf7c29e2473e8715b9ca0d2e4f98356e
Kyle_Tecson <kyltecs@gmail.com>
2019-03-22 12:04:23 -0700

    removed screen from app.js

--- client/js/App.js
+++ client/js/App.js
@@ -3,17 +3,14 @@ import { StatusBar } from "react-native";
 import client from "./config/api";
 import { ApolloProvider } from "react-apollo";
 import RootStackNavigator from "./navigation/RootStackNavigator";
-import UpdateProfileModel from "./components/UpdateProfileModel";
 
 export default class App extends Component {
   render() {
-    // StatusBar.setBarStyle("light-content");
+    StatusBar.setBarStyle("light-content");
     return (
-      // <ApolloProvider client={client}>
-      //   <RootStackNavigator />
-      // </ApolloProvider>
-
-      <UpdateProfileModel />
+      <ApolloProvider client={client}>
+        <RootStackNavigator />
+      </ApolloProvider>
     );
   }
 }

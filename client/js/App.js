5cad04b0e7aa14342ccbeae8dda7b0467b80c450
Kyle_Tecson <kyltecs@gmail.com>
2019-03-22 10:47:04 -0700

    remove error model from app.js

--- client/js/App.js
+++ client/js/App.js
@@ -3,17 +3,14 @@ import { StatusBar } from "react-native";
 import client from "./config/api";
 import { ApolloProvider } from "react-apollo";
 import RootStackNavigator from "./navigation/RootStackNavigator";
-import ErrorModel from "./components/ErrorModel";
 
 export default class App extends Component {
   render() {
-    // StatusBar.setBarStyle('light-content');
+    StatusBar.setBarStyle("light-content");
     return (
-      // <ApolloProvider client={client}>
-      //   <RootStackNavigator />
-      // </ApolloProvider>
-
-      <ErrorModel />
+      <ApolloProvider client={client}>
+        <RootStackNavigator />
+      </ApolloProvider>
     );
   }
 }

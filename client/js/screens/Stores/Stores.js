4b3b286852e0221ae0002e5c6c3cf186b059366a
nejmal <lam.jennifer.ky@gmail.com>
2019-03-25 16:01:49 -0700

    Add get distance formula for stores and user location.

--- client/js/screens/Stores/Stores.js
+++ client/js/screens/Stores/Stores.js
@@ -14,7 +14,7 @@ import PropTypes from 'prop-types';
 import Subheader from '../../components/Subheader';
 import CustomText from '../../components/CustomText';
 import { renderSeparator } from '../../lib/helpers/separator';
-import { colors, center, row, padding } from '../../config/styles';
+import { colors, center, row, padding, h3 } from '../../config/styles';
 import styles from './styles';
 
 class Stores extends Component {
@@ -26,16 +26,9 @@ class Stores extends Component {
         latitude: 49.26473,
         latitudeDelta: 0.004,
         longitudeDelta: 0.004
-      }
-
-      // focusedLocation: {
-      //   latitude: 49.26473,
-      //   longitude: -123.099305,
-      //   latitudeDelta: 0.003,
-      //   longitudeDelta:
-      //     (Dimensions.get('window').width / Dimensions.get('window').height) *
-      //     0.003
-      // }
+      },
+      currentLatitude: null,
+      currentLongitude: null
     };
   }
 
@@ -76,7 +69,14 @@ class Stores extends Component {
                 Dimensions.get('window').height) *
               0.003
           };
+          const latitude = position.coords.latitude;
+          console.log('findCoordinates() LATITUDE', latitude);
+          const longitude = position.coords.longitude;
+          console.log('findCoordinates() LONGITUDE', longitude);
+
           this.setState({ focusedLocation: location });
+          this.setState({ currentLatitude: latitude });
+          this.setState({ currentLongitude: longitude });
         },
         error => Alert.alert(error.message),
         { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
@@ -122,9 +122,32 @@ class Stores extends Component {
     this.map.animateToRegion(this.region, 100);
   };
 
+  getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+    const R = 6371; // Radius of the earth in km
+    const dLat = this.deg2rad(lat2 - lat1); // deg2rad below
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) *
+        Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+
+    console.log(d);
+    return d;
+  };
+
+  deg2rad = deg => {
+    return deg * (Math.PI / 180);
+  };
+
   render() {
     const { stores } = this.props;
     console.log('STORES', stores);
+
+    const { currentLatitude, currentLongitude } = this.state;
 
     return (
       <View style={styles.container}>
@@ -259,38 +282,63 @@ class Stores extends Component {
             </MapView>
           )}
         </View>
+        {this.state.focusedLocation && (
+          <FlatList
+            data={stores}
+            renderItem={({ item }) => {
+              const lat1 = this.state.focusedLocation.latitude;
+              const lon1 = this.state.focusedLocation.longitude;
+              const lat2 = item.lat;
+              const lon2 = item.long;
 
-        <FlatList
-          data={stores}
-          renderItem={({ item }) => (
-            <View style={styles.storeItem}>
-              <TouchableHighlight
-                underlayColor={colors.neutralLight}
-                onPress={() => {
-                  const newRegion = {
-                    longitude: parseFloat(item.long),
-                    latitude: parseFloat(item.lat),
-                    latitudeDelta: 0.004,
-                    longitudeDelta: 0.004
-                  };
-                  this.setState({ focusedLocation: newRegion });
-                }}
-              >
-                <Fragment>
-                  <CustomText style={styles.bold}>{item.name}</CustomText>
-                  <CustomText style={styles.body}>{item.address}</CustomText>
-                  <CustomText style={styles.body}>{`${item.city}, ${
-                    item.province
-                  }`}</CustomText>
-                  <CustomText style={styles.bold}>{item.hours}</CustomText>
-                </Fragment>
-              </TouchableHighlight>
-            </View>
-          )}
-          keyExtractor={(item, index) => item + index}
-          ItemSeparatorComponent={renderSeparator}
-          style={styles.storesWrapper}
-        />
+              console.log('LAT1', lat1);
+              console.log('LON1', lon1);
+              console.log('LAT2', lat2);
+              console.log('LON2', lon2);
+
+              const distance = Math.round(
+                this.getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2)
+              ).toFixed(2);
+
+              return (
+                <View style={styles.storeItem}>
+                  <TouchableHighlight
+                    underlayColor={colors.neutralLight}
+                    onPress={() => {
+                      const newRegion = {
+                        longitude: parseFloat(item.long),
+                        latitude: parseFloat(item.lat),
+                        latitudeDelta: 0.004,
+                        longitudeDelta: 0.004
+                      };
+                      this.setState({ focusedLocation: newRegion });
+                    }}
+                  >
+                    <Fragment>
+                      <CustomText style={styles.bold}>{item.name}</CustomText>
+                      <CustomText style={styles.body}>
+                        {item.address}
+                      </CustomText>
+                      <CustomText style={styles.body}>{`${item.city}, ${
+                        item.province
+                      }`}</CustomText>
+                      <CustomText style={styles.bold}>{item.hours}</CustomText>
+                    </Fragment>
+                  </TouchableHighlight>
+                  <View style={[styles.distanceWrapper, { ...row, ...center }]}>
+                    <CustomText style={[styles.distance, { ...h3 }]}>
+                      {distance}
+                    </CustomText>
+                    <CustomText style={styles.body}>km</CustomText>
+                  </View>
+                </View>
+              );
+            }}
+            keyExtractor={(item, index) => item + index}
+            ItemSeparatorComponent={renderSeparator}
+            style={styles.storesWrapper}
+          />
+        )}
       </View>
     );
   }

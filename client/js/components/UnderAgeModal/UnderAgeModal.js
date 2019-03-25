0b69a74012ed4a9533c8a5f99779e94cfea966c8
Kyle_Tecson <kyltecs@gmail.com>
2019-03-25 07:37:58 -0700

    set up template for modal

---
+++ client/js/components/UnderAgeModal/UnderAgeModal.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class UnderAgeModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default UnderAgeModal;

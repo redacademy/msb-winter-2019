b9c5d873f58e0a385ff010517612769f43f576e2
alexfts <alxfetisova@gmail.com>
2019-03-25 13:32:39 -0700

    Remove extra styling

--- client/js/screens/Profile/styles.js
+++ client/js/screens/Profile/styles.js
@@ -4,9 +4,7 @@ import { colors, fonts } from '../../config/styles';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    height: '100%',
-    width: '100%'
+    alignItems: 'center'
   },
   imageWrapper: {
     flexGrow: 1.5,

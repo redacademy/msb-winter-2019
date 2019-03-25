755690f9ca0f530baf3b51b08397a9bcd9b2a707
Kyle_Tecson <kyltecs@gmail.com>
2019-03-25 07:48:03 -0700

    add content and some styles to modal

--- client/js/components/UnderAgeModal/UnderAgeModal.js
+++ client/js/components/UnderAgeModal/UnderAgeModal.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
-class UnderAgeModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
+const UnderAgeModal = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <View />
+        <Text style={styles.title}>SORRY!</Text>
+        <TouchableOpacity>
+          <Image source={require("../../assets/images/Icons/exit_icon.png")} />
+        </TouchableOpacity>
       </View>
-    );
-  }
-}
+
+      <Image
+        style={styles.beer}
+        source={require("../../assets/images/Icons/lock_growler_icon.png")}
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>
+          Please come again when you are of legal age to drink!
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 export default UnderAgeModal;
--- client/js/components/UnderAgeModal/styles.js
+++ client/js/components/UnderAgeModal/styles.js
@@ -1,5 +1,38 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.black,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    width: Dimensions.get("window").width * 0.6
+  },
+
+  title: {
+    color: colors.white,
+    fontFamily: fonts.secondary,
+    fontSize: fonts.xxl,
+    paddingLeft: 20
+  },
+  beer: {
+    marginTop: 20,
+    marginBottom: 20
+  },
+  textContainer: {
+    marginTop: 20
+  },
+  text: {
+    color: colors.white,
+    fontFamily: fonts.primarySemi,
+    fontSize: fonts.md,
+    alignSelf: "center"
+  }
+});
 
 export default styles;

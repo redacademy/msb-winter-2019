d8de1c1a8cfacb9791385334d690df0d00f230fc
nejmal <lam.jennifer.ky@gmail.com>
2019-03-26 09:45:22 -0700

    Make mapview appear on load.

--- client/js/screens/Stores/Stores.js
+++ client/js/screens/Stores/Stores.js
@@ -14,15 +14,7 @@ import PropTypes from 'prop-types';
 import Subheader from '../../components/Subheader';
 import CustomText from '../../components/CustomText';
 import { renderSeparator } from '../../lib/helpers/separator';
-import {
-  colors,
-  center,
-  row,
-  padding,
-  h3,
-  jusBtwnRow,
-  w100
-} from '../../config/styles';
+import { colors } from '../../config/styles';
 import styles from './styles';
 
 class Stores extends Component {
@@ -30,10 +22,10 @@ class Stores extends Component {
     super(props);
     this.state = {
       region: {
-        longitude: -123.099305,
-        latitude: 49.26473,
-        latitudeDelta: 0.004,
-        longitudeDelta: 0.004
+        // longitude: -123.099305,
+        // latitude: 49.26473,
+        // latitudeDelta: 0.004,
+        // longitudeDelta: 0.004
       }
     };
   }
@@ -53,7 +45,7 @@ class Stores extends Component {
     //   //   // latitudeDelta: 0.00522,
     //   //   latitudeDelta: 0.003,
     //   //   longitudeDelta:
-    //   //     (Dimensions.get('window').width / Dimensions.get('window').height) *
+    //   //     (Dimensions.get('window').width / Dimensions.get\('window').height) *
     //   //     0.003
     //   //   // 0.00522
     //   // }
@@ -164,122 +156,120 @@ class Stores extends Component {
       });
     console.log('STORES', stores);
 
-    const { currentLatitude, currentLongitude } = this.state;
-
     return (
       <View style={styles.container}>
         <Subheader>Stores</Subheader>
         <View style={styles.mapWrapper}>
-          {this.state.region.longitude && (
-            <MapView
-              style={styles.map}
-              // initialRegion={this.state.region}
-              // region={this.state.region}
-              followsUserLocation={true}
-              showsUserLocation={true}
-              showsMyLocationButton={true}
-              loadingEnabled={true}
-              zoomEnabled={true}
-              zoomControlEnabled={true}
-              loadingIndicatorColor='#fea405'
-              loadingBackgroundColor='#ffffff'
-              region={this.state.focusedLocation}
-              onPress={this.pickLocationHandler}
-              // customMapStyle={mapStyle}
-              ref={ref => (this.map = ref)}
-            >
-              <View
-                style={[
-                  styles.zoom,
-                  {
-                    // ...row
+          {/* {this.state.region.longitude && ( */}
+          <MapView
+            style={styles.map}
+            // initialRegion={this.state.region}
+            // region={this.state.region}
+            followsUserLocation={true}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            loadingEnabled={true}
+            zoomEnabled={true}
+            zoomControlEnabled={true}
+            loadingIndicatorColor='#fea405'
+            loadingBackgroundColor='#ffffff'
+            region={this.state.focusedLocation}
+            onPress={this.pickLocationHandler}
+            // customMapStyle={mapStyle}
+            ref={ref => (this.map = ref)}
+          >
+            <View
+              style={[
+                styles.zoom,
+                {
+                  // ...row
 
-                    position: 'absolute',
-                    right: 0,
-                    bottom: 0
-                    // justifyContent: 'space-between'
-                    // alignItems: 'flex-end'
-                    // paddingVertical: padding.xxxs,
-                    // paddingHorizontal: padding.xxxs
-                  }
-                ]}
+                  position: 'absolute',
+                  right: 0,
+                  bottom: 0
+                  // justifyContent: 'space-between'
+                  // alignItems: 'flex-end'
+                  // paddingVertical: padding.xxxs,
+                  // paddingHorizontal: padding.xxxs
+                }
+              ]}
+            >
+              <TouchableOpacity
+                style={styles.mapIcon}
+                onPress={() => {
+                  this.zoomIn();
+                }}
               >
-                <TouchableOpacity
-                  style={styles.mapIcon}
-                  onPress={() => {
-                    this.zoomIn();
+                <Ionicons
+                  name={Platform.select({
+                    android: 'md-add-circle',
+                    ios: 'ios-add-circle'
+                  })}
+                  size={35}
+                  color={colors.brand}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.mapIcon}
+                onPress={() => {
+                  this.zoomOut();
+                }}
+              >
+                <Ionicons
+                  name={Platform.select({
+                    android: 'md-remove-circle',
+                    ios: 'ios-remove-circle'
+                  })}
+                  size={35}
+                  color={colors.brand}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.mapIcon}
+                onPress={() => {
+                  this.findCoordinates();
+                }}
+              >
+                <Ionicons
+                  name={Platform.select({
+                    android: 'md-navigate',
+                    ios: 'ios-navigate'
+                  })}
+                  size={35}
+                  color={colors.black}
+                />
+              </TouchableOpacity>
+            </View>
+            {stores.map(store => {
+              return (
+                <Marker
+                  key={store.id}
+                  coordinate={{
+                    latitude: parseFloat(store.lat),
+                    longitude: parseFloat(store.long)
                   }}
+                  title={store.name}
+                  image={require('../../assets/images/Icons/point_location.png')}
+                  onPress={e => console.log(e.nativeEvent)}
                 >
-                  <Ionicons
-                    name={Platform.select({
-                      android: 'md-add-circle',
-                      ios: 'ios-add-circle'
-                    })}
-                    size={35}
-                    color={colors.brand}
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.mapIcon}
-                  onPress={() => {
-                    this.zoomOut();
-                  }}
-                >
-                  <Ionicons
-                    name={Platform.select({
-                      android: 'md-remove-circle',
-                      ios: 'ios-remove-circle'
-                    })}
-                    size={35}
-                    color={colors.brand}
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.mapIcon}
-                  onPress={() => {
-                    this.findCoordinates();
-                  }}
-                >
-                  <Ionicons
-                    name={Platform.select({
-                      android: 'md-navigate',
-                      ios: 'ios-navigate'
-                    })}
-                    size={35}
-                    color={colors.black}
-                  />
-                </TouchableOpacity>
-              </View>
-              {stores.map(store => {
-                return (
-                  <Marker
-                    key={store.id}
-                    coordinate={{
-                      latitude: parseFloat(store.lat),
-                      longitude: parseFloat(store.long)
+                  <View
+                    style={{
+                      height: 30,
+                      top: -5,
+                      left: 26,
+                      zIndex: 2,
+                      position: 'absolute'
                     }}
-                    title={store.name}
-                    image={require('../../assets/images/Icons/point_location.png')}
-                    onPress={e => console.log(e.nativeEvent)}
                   >
-                    <View
-                      style={{
-                        height: 30,
-                        top: -5,
-                        left: 26,
-                        zIndex: 2,
-                        position: 'absolute'
-                      }}
-                    >
-                      <CustomText style={styles.markerTitle}>
-                        {store.name}
-                      </CustomText>
-                    </View>
-                  </Marker>
-                );
-              })}
-            </MapView>
-          )}
+                    <CustomText style={styles.markerTitle}>
+                      {store.name}
+                    </CustomText>
+                  </View>
+                </Marker>
+              );
+            })}
+          </MapView>
+          {/* )} */}
         </View>
         {this.state.focusedLocation && (
           <FlatList

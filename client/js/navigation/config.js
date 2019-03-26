c884767eac9d670090b70cf1aae402f924640fd3
alexfts <alxfetisova@gmail.com>
2019-03-26 09:49:21 -0700

    Put back button for drawer navigator

--- client/js/navigation/config.js
+++ client/js/navigation/config.js
@@ -1,83 +1,77 @@
 import React from 'react';
 import { Image, Platform } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 import { colors } from '../config/styles';
 
-export const headerLeftFromDrawer = navigation => (
-  <Ionicons
-    name={
-      ['Your Events', 'FavBeers', 'Contact', 'Profile'].includes(
-        navigation.state.routeName
-      )
-        ? Platform.select({
-            ios: 'ios-arrow-dropleft-circle',
-            android: 'md-arrow-dropleft-circle'
-          })
-        : Platform.select({
-            ios: 'ios-menu',
-            android: 'md-menu'
-          })
-    }
-    onPress={() => {
-      if (
-        ['Your Events', 'FavBeers', 'Contact', 'Profile'].includes(
-          navigation.state.routeName
-        )
-      ) {
-        navigation.goBack();
+export const headerLeftFromDrawer = navigation => {
+  const drawerRoutes = ['FavBeers', 'Contact', 'Profile'];
+  return (
+    <Ionicons
+      name={
+        drawerRoutes.includes(navigation.state.routeName)
+          ? Platform.select({
+              ios: 'ios-arrow-dropleft-circle',
+              android: 'md-arrow-dropleft-circle'
+            })
+          : Platform.select({
+              ios: 'ios-menu',
+              android: 'md-menu'
+            })
       }
-      navigation.toggleDrawer();
-    }}
-    size={40}
-    color={'white'}
-    style={{ marginLeft: 20 }}
-  />
-);
-
-export const sharedNavigationOptions = navigation => {
-  console.log('sharedNavigationOptions', navigation);
-  return {
-    headerTitle: (
-      <Image
-        source={require('../assets/images/Logos/msb_logo_white.png')}
-        style={{ resizeMode: 'contain' }}
-      />
-    ),
-    headerBackTitle: null,
-    headerRight: (
-      <Image
-        source={require('../assets/images/Icons/notifications_icon_inactive.png')}
-        style={{ resizeMode: 'contain', marginRight: 10 }}
-      />
-    ),
-    headerLeft: () => (
-      <Ionicons
-        name={
-          navigation.state.routeName === 'Event'
-            ? Platform.select({
-                ios: 'ios-arrow-dropleft-circle',
-                android: 'md-arrow-dropleft-circle'
-              })
-            : Platform.select({
-                ios: 'ios-menu',
-                android: 'md-menu'
-              })
+      onPress={() => {
+        if (drawerRoutes.includes(navigation.state.routeName)) {
+          navigation.goBack();
+        } else {
+          navigation.toggleDrawer();
         }
-        onPress={() => {
-          navigation.state.routeName === 'Event'
-            ? navigation.goBack()
-            : navigation.toggleDrawer();
-        }}
-        size={40}
-        color={'white'}
-        style={{ marginLeft: 20 }}
-      />
-    ),
-    headerStyle: {
-      backgroundColor: colors.black,
-      height: 75,
-      borderBottomWidth: 0
-    }
-  };
+      }}
+      size={40}
+      color={'white'}
+      style={{ marginLeft: 20 }}
+    />
+  );
 };
+
+export const sharedNavigationOptions = navigation => ({
+  headerTitle: (
+    <Image
+      source={require('../assets/images/Logos/msb_logo_white.png')}
+      style={{ resizeMode: 'contain' }}
+    />
+  ),
+  headerBackTitle: null,
+  headerRight: (
+    <Image
+      source={require('../assets/images/Icons/notifications_icon_inactive.png')}
+      style={{ resizeMode: 'contain', marginRight: 10 }}
+    />
+  ),
+  headerLeft: () => (
+    <Ionicons
+      name={
+        navigation.state.routeName === 'Event'
+          ? Platform.select({
+              ios: 'ios-arrow-dropleft-circle',
+              android: 'md-arrow-dropleft-circle'
+            })
+          : Platform.select({
+              ios: 'ios-menu',
+              android: 'md-menu'
+            })
+      }
+      onPress={() => {
+        navigation.state.routeName === 'Event'
+          ? navigation.goBack()
+          : navigation.toggleDrawer();
+      }}
+      size={40}
+      color={'white'}
+      style={{ marginLeft: 20 }}
+    />
+  ),
+  headerStyle: {
+    backgroundColor: colors.black,
+    height: 75,
+    borderBottomWidth: 0
+  }
+});

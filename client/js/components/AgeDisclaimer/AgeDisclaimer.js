ada8f4e23c61d2f9caa39117fd4a07999f266d9a
alexfts <alxfetisova@gmail.com>
2019-03-27 10:17:44 -0700

    Age disclaimer redirect to login

--- client/js/components/AgeDisclaimer/AgeDisclaimer.js
+++ client/js/components/AgeDisclaimer/AgeDisclaimer.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import { withNavigation } from "react-navigation";
-import PropTypes from "prop-types";
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import PropTypes from 'prop-types';
 
-import styles from "./styles";
+import styles from './styles';
 
 const AgeDisclaimer = ({ navigation }) => {
   return (
     <View style={styles.constainer}>
       <Image
         style={styles.image}
-        source={require("../../assets/images/Icons/star_bottle.png")}
+        source={require('../../assets/images/Icons/star_bottle.png')}
       />
       <Text style={styles.text}>
         Hang on my friend, you must be over 19 years old to use this app under
@@ -18,7 +18,7 @@ const AgeDisclaimer = ({ navigation }) => {
       </Text>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Signup")}
+        onPress={() => navigation.navigate('Signin')}
       >
         <Text style={styles.buttonText}>Enter</Text>
       </TouchableOpacity>
